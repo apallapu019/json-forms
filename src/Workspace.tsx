dda88ab8375a43dbc022d719d3f84e47e3f1880e
avinash pallapu <124399482+apallapu019@users.noreply.github.com>
2023-05-08 12:26:18 -0400

    init commit
    init commit

---
+++ src/Workspace.tsx
@@ -0,0 +1,111 @@
+import { Fragment, useState, useMemo } from 'react';
+import { JsonForms } from '@jsonforms/react';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import './App.css';
+import datamigrworkspace from './datamigrworkspace.json';
+import datamigrworkspaceui from './datamigrworkspaceui.json';
+import {
+  materialCells,
+  materialRenderers,
+} from '@jsonforms/material-renderers';
+import RatingControl from './RatingControl';
+import ratingControlTester from './ratingControlTester';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  container: {
+    padding: '1em',
+    width: '100%',
+  },
+  title: {
+    textAlign: 'center',
+    padding: '0.25em',
+  },
+  dataContent: {
+    display: 'flex',
+    justifyContent: 'center',
+    borderRadius: '0.25em',
+    backgroundColor: '#cecece',
+    marginBottom: '1rem',
+  },
+  resetButton: {
+    margin: 'auto !important',
+    display: 'block !important',
+  },
+  demoform: {
+    margin: 'auto',
+    padding: '1rem',
+  },
+});
+
+const initialData = {
+  title: 'Data Migration Workspace',
+  description: 'Confirm if yds ...',
+  lineOfBusiness: 'IT'
+};
+
+const renderers = [
+  ...materialRenderers,
+  //register custom renderers
+  { tester: ratingControlTester, renderer: RatingControl },
+];
+
+const Workspace = () => {
+  const classes = useStyles();
+  const [data, setData] = useState<any>(initialData);
+  const stringifiedData = useMemo(() => JSON.stringify(data, null, 2), [data]);
+
+  const clearData = () => {
+    setData({});
+  };
+
+  return (
+    <Fragment>
+      <div className='App'>
+      </div>
+
+      <Grid
+        container
+        justifyContent={'center'}
+        spacing={1}
+        className={classes.container}
+      >
+        <Grid item sm={6}>
+          <Typography variant={'h4'} className={classes.title}>
+            Data Migration Workspace
+          </Typography>
+          <div className={classes.dataContent}>
+            <pre id='boundData'>{stringifiedData}</pre>
+          </div>
+          <Button
+            className={classes.resetButton}
+            onClick={clearData}
+            color='primary'
+            variant='contained'
+          >
+            Clear data
+          </Button>
+        </Grid>
+        <Grid item sm={6}>
+          <Typography variant={'h4'} className={classes.title}>
+            Data Migration Workspace
+          </Typography>
+          <div className={classes.demoform}>
+            <JsonForms
+              schema={datamigrworkspace}
+              uischema={datamigrworkspaceui}
+              data={data}
+              renderers={renderers}
+              cells={materialCells}
+              onChange={({ errors, data }) => setData(data)}
+            />
+          </div>
+        </Grid>
+      </Grid>
+    </Fragment>
+  );
+};
+
+export default Workspace;
